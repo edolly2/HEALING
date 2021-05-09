5f518331d8ce1d0c5ba1a0779411162fb699f335
edolly2 <edolly2@protonmail.com>
2021-05-09 02:54:52 -0500

    ""

--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
 $(document).ready( () => {
-    $('.show-more').on('click', () => {
+
+    $('.home').addClass('is-active');
+    $('#home').children().show();
+
+    $('.fa-bars').on('click', () => {
         $('aside').show(1000);
-        $('.menu-drop').hide(1000);
+        $('.fa-bars').hide();
     })
     
     $('.exit').on('click', () => {
         $('aside').hide(1000);
-        $('.menu-drop').show(1000);
+        $('.fa-bars').delay(1000).fadeIn(1000);
     });
 
-    $(':not(aside)').on('click', () => {
+    $('.side-list li').on('click', (event) => {
+        $('.side-list li').removeClass('is-active');
+        $('.dash').hide();
+        $(event.currentTarget).children().show();
+        $(event.currentTarget).addClass('is-active');
         $('aside').hide(1000);
-        $('.menu-drop').show(1000);
-    })
+        $('.fa-bars').delay(1000).fadeIn(1000);
+    });
+
+    // $('.home').on('click', () => {
+    //     $('.main-contact').hide();
+    //     // $('header').hide();
+    //     // $('footer').hide();
+    //     $('.main-home').fadeIn(2000);
+    // })
+
+    // $('.contact').on('click', () => {
+    //     $('.main-home').hide();
+    //     $('header').hide();
+    //     $('footer').hide();
+    //     $('.main-contact').fadeIn(2000);
+    // })
+
+
+    // $(':not(aside)').on('click', () => {
+    //     $('aside').hide(1000);
+    //     $('.menu-drop').show(1000);
+    // })
 });
 
